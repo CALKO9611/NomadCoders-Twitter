6297c93df9b9da2e174652d25dee9d3c2cc5e41b
CALKO9611 <kimd9669@naver.com>
2023-11-26 17:48:36 +0900

    #2.3 Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCQk_Hs8dU6fHfqXBEdpzGrb4j-66YJNjc",
+  authDomain: "nomadcoders-twiiter.firebaseapp.com",
+  projectId: "nomadcoders-twiiter",
+  storageBucket: "nomadcoders-twiiter.appspot.com",
+  messagingSenderId: "939615256380",
+  appId: "1:939615256380:web:4840b6d2ff56ecd3686839",
+};
+
+const app = initializeApp(firebaseConfig);
