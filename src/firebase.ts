f551fc6a185471017ce11dd9817bc5bfbc60855b
CALKO9611 <kimd9669@naver.com>
2023-11-26 18:14:29 +0900

    #3.0 Setup

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCQk_Hs8dU6fHfqXBEdpzGrb4j-66YJNjc",
@@ -10,3 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
