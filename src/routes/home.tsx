f319ddf06f84a498ea17ad6c100a7537c0298e19
CALKO9611 <kimd9669@naver.com>
2023-11-27 16:29:12 +0900

    #3.3 Protected Routes

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,13 @@
+import { auth } from "../firebase";
+
+const logOut = () => {
+  auth.signOut();
+};
+
 export default function Home() {
-  return <h1>Home!</h1>;
+  return (
+    <h1>
+      <button onClick={logOut}>Log Out</button>
+    </h1>
+  );
 }
